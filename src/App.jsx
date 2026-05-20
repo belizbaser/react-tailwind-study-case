a2e8b821dd36ca48047c45ef736480605c556c54
belizbaser <belizbaser@icloud.com>
2026-05-20 20:53:10 +0300

    Initial project setup with React and TailwindCSS

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <main className="min-h-screen bg-[#f5f1f1] p-10">
+      <h1 className="text-5xl">
+        Study Case 
+      </h1>
+    </main>
+  )
+}
+
+export default App
